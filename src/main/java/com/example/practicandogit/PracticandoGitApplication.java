3f8d7e7b44fec9457d2267382390c211d1089939
unknown <luis.ramost@pucp.edu.pe>
2023-04-03 19:03:43 -0500

    primer commit

---
+++ src/main/java/com/example/practicandogit/PracticandoGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicandogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticandoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticandoGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicandogit/PracticandoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicandogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticandoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
